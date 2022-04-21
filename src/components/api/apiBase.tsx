6bb02d69449441ba7fd38fe220f102ef4d46bb7a
Thomas Zhou <50957137+thomaszhou01@users.noreply.github.com>
2022-04-20 21:09:10 -0400

    Update apiBase.tsx

--- src/components/api/apiBase.tsx
+++ src/components/api/apiBase.tsx
@@ -1,8 +1,8 @@
 const axios = require("axios");
 export default axios.create({
   timeout: 0,
-  baseURL: "http://127.0.0.1:8000/",
-  // baseURL: "https://reddit-sentiment-backend.herokuapp.com/",
+  // baseURL: "http://127.0.0.1:8000/",
+  baseURL: "https://reddit-sentiment-backend.herokuapp.com/",
   // headers: {
   //   "Access-Control-Allow-Origin": "*",
   // },
